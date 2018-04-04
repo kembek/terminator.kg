894e8b7dd212634e900fa33f8ffd92e9e01c882a
Stas <de1inmur@gmail.com>
2018-04-04 17:14:21 +0600

    update

---
+++ resources/pages/FAQ.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="FAQ-Wrapper">
+  <Search />
+</div>
+</template>
+
+<script>
+import Search from '~/components/FAQ/FAQ-Search'
+export default {
+  components:{
+    Search
+  }
+}
+</script>
+<style lang="less">
+.FAQ-Wrapper{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
