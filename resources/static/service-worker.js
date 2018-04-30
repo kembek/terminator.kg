ce40b37b6fde6b867056f39787f9c7535c23bfbc
Evald24 <evaldsmalyakov@mail.ru>
2018-04-30 12:18:01 +0600

    update

--- resources/static/service-worker.js
+++ resources/static/service-worker.js
@@ -1,9 +1,9 @@
   if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
+    alert("Will the service worker register?");
     navigator.serviceWorker.register('service-worker.js')
       .then(function(reg){
-        console.log("Yes, it did.");
+        alert("Yes, it did.");
       }).catch(function(err) {
-        console.log("No it didn't. This happened: ", err)
+        alert("No it didn't. This happened: ", err)
       });
   }
