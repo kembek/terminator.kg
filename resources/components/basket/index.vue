8ed4a1dd8d9630799dee6f8b1cc36fdf48eb2151
Stas <de1inmur@gmail.com>
2018-05-03 11:12:14 +0600

    basket

---
+++ resources/components/basket/index.vue
@@ -0,0 +1,49 @@
+
+<template>
+<div class="basket">
+  <div class="panel">
+    <div class="top">
+      Корзина покупок
+    </div>
+    <div class="content">
+      <Item v-for="(item, i) in items" :key="i" :item="item" />
+    </div>
+    <nuxt-link to="/basket/" class="link">
+      Перейти в корзину
+    </nuxt-link>
+  </div>
+</div>
+</template>
+
+<script>
+import Item from './item'
+export default {
+  data() {
+    return {
+      items: [{
+        id: 0,
+        title: 'Redmi',
+        count: 5,
+        price: 12000,
+        color: 0,
+        image: 'hits.jpg'
+      }, {
+        id: 1,
+        title: 'Redmi2',
+        count: 2,
+        price: 12000,
+        color: 0,
+        image: 'hits.jpg'
+      }]
+    }
+  },
+  methods: {},
+  components: {
+    Item
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ resources/components/basket/item.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="item">
+  <img :src="'/images/' + item.image" />
+  <div class="description">
+    <h3>{{item.title}}</h3>
+    <span class="count">{{item.count}}</span>
+    <span class="price">{{item.price}}</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {}
+}
+</script>
